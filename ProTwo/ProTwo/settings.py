650e31d1e2e4aff6fc7ae5059ff5813120b7cafd
gk90731 <36289654+gk90731@users.noreply.github.com>
2020-05-27 15:06:37 +0530

    Update settings.py

--- ProTwo/ProTwo/settings.py
+++ ProTwo/ProTwo/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ['technohubbit.pythonanywhere.com','*']
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'bit.technohub@gmail.com'
-EMAIL_HOST_PASSWORD = 'GStechnohubbit.in'
+EMAIL_HOST_PASSWORD = '**********'
 EMAIL_PORT = 587
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
